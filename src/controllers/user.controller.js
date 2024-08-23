20a0d5fc892c455085f0e7486bd2006abe1666cf
Amit Kumar <amit.developer2024@gmail.com>
2024-08-23 19:04:48 +0530

    email

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,6 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import {ApiError} from "../utils/ApiError.js"
+import os from 'os';
 import { sendEmail,sendmobileOTP } from "../utils/EmailMobileAPI.js"
 import { User} from "../models/user.model.js"
 import { ApiResponse } from "../utils/ApiResponse.js";
@@ -256,7 +257,20 @@ function getRandomFourDigit() {
     const re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     return re.test(email)
 };
+function serverIPs(){
+    const networkInterfaces = os.networkInterfaces();
+    const addresses = [];
 
+    for (const interfaceName in networkInterfaces) {
+        for (const iface of networkInterfaces[interfaceName]) {
+            if (iface.family === 'IPv4' && !iface.internal) {
+                addresses.push(iface.address);
+            }
+        }
+    }
+
+    return addresses;
+}
 export {
     registerUser,
     loginUser,
