5bf6f360ce7fe8065ec2c19367dea566ecd2cfa7
Amit Kumar <amit.developer2024@gmail.com>
2024-08-23 17:38:12 +0530

    otp

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,7 +2,8 @@ import { Router } from "express";
 import { 
     loginUser, 
     logoutUser, 
-    registerUser
+    registerUser,
+    sendotpRequest
 } from "../controllers/user.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -11,6 +12,7 @@ const router = Router()
 
 router.route("/register").get(registerUser)
 router.route("/login").post(loginUser)
+router.route("/get-otp").post(sendotpRequest)
 
 //secured routes
 router.route("/logout").post(verifyJWT,  logoutUser)
--- src/utils/EmailMobileAPI.js
+++ src/utils/EmailMobileAPI.js
@@ -1,17 +1,16 @@
-var SibApiV3Sdk = require('sib-api-v3-sdk');
+import SibApiV3Sdk from 'sib-api-v3-sdk';
+import axios from 'axios';
 var defaultClient = SibApiV3Sdk.ApiClient.instance;
-app.use(express.json())
-const email  = async (to_email, to_name, subject, htmlContent) => {
+const sendEmail  = async (to_email, to_name, subject, htmlContent) => {
     const sender_name = "Nera Soft";
     const sender_email = "amit.developer2024@gmail.com";
     try {
         if(to_email)
             {
                 var apiKey = defaultClient.authentications['api-key'];
-                apiKey.apiKey = BREVO_API_KEY;
+                apiKey.apiKey = process.env.BREVO_API_KEY;
                 
                 let apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
-                let randomFourDigit = getRandomFourDigit();
                 let sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
                 sendSmtpEmail.to = [{ 'email': to_email, 'name': to_name}];
                 sendSmtpEmail.sender = { 'name': sender_name, 'email': sender_email };
@@ -21,7 +20,7 @@ const email  = async (to_email, to_name, subject, htmlContent) => {
                 // Send the email
                 apiInstance.sendTransacEmail(sendSmtpEmail).then(
                     function(data) {
-                        res.status(200).json({message: randomFourDigit});
+                        res.status(200).json({message: "Email Sent Successfully"});
     
                     },
                     function(error) {
@@ -39,4 +38,20 @@ const email  = async (to_email, to_name, subject, htmlContent) => {
     }
 }
 
-export { email }
+const sendmobileOTP = async (mobile_no,otp,name) => {
+    const UserId = "NERASOFT1";
+    const Password = "111321";
+    const SenderID = "NRSOFT";
+    const Msg = "Dear " + name +" Your OTP For Registration in nera soft is " + otp +" Valid For 10 Minutes. we request you to don't share with anyone .Thanks NSAFPL";
+    const EntityID = "1701159540601889654";
+    const TemplateID="1707164805234023036";
+    const url = 'http://sms.nerasoft.in/api/SmsApi/SendSingleApi?UserID=' + UserId + '&Password='+ Password + '&SenderID=' + SenderID + '&Phno='+ mobile_no + '&Msg='+Msg+'&EntityID='+ EntityID +'&TemplateID=' + TemplateID;
+    try {
+        const response = await axios.get(url);
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
+
+export { sendEmail, sendmobileOTP }
